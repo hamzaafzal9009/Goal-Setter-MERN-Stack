452926ff79ba9ecaafce52dcc3f42d8f72619007
hamzaafzal <hamza@syncitgroup.com>
2022-08-05 18:52:03 +0500

    env added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require('express')
+const devenv = require('dotenv').config()
+
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
