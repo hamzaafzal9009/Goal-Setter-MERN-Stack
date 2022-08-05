fcc58ab036a11c60aeb89e2bc94d2d7ae6fcc51f
hamzaafzal <hamza@syncitgroup.com>
2022-08-05 19:05:04 +0500

    routes added

--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -1,7 +1,18 @@
 const express = require("express");
 
 const router = express.Router();
+
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Getting goals..." });
 });
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "setting goals..." });
+});
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `get goals... ${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `deleting goal... ${req.params.id}` });
+});
+
 module.exports = router;
