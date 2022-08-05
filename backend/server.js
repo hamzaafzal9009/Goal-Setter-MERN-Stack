96e078e56c35234ea61debecbbde6450ce446b05
hamzaafzal <hamza@syncitgroup.com>
2022-08-05 18:57:51 +0500

    Get Goals Route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+
+app.get('/api/goals', (req, res) =>  {
+    res.status(200).json({message : "Getting goals..."})
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
