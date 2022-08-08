da699a2ba9542a145fb25f57e245b24374ce7139
hamzaafzal <hamza@syncitgroup.com>
2022-08-08 18:17:44 +0500

    async-handler added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,53 +1,47 @@
+const asyncHandler = require("express-async-handler");
 // @desc Get Goals
 // @route GET /api/goals/
 // @access Private
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Getting goals..." });
-};
-
+});
 
 // @desc Get Goals by ID
 // @route GET /api/goals/:id
 // @access Private
 
-const getGoal = (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `get goal... ${req.params.id}` });
-};
-
+});
 
 // @desc Set Goals
 // @route POST /api/goals
 // @access Private
 
-const setGoal = (req, res) => {
+const setGoal =  asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
-    throw new Error(`Invalid JSON response  from server`);  
+    throw new Error(`Invalid JSON response  from server`);
   }
   res.status(200).json({ message: `Setting goal...}` });
-};
-
-
+});
 
 // @desc Update Goals
 // @route Put /api/goals/id
 // @access Private
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Updating goal... ${req.params.id}}` });
-};
-
-
+});
 
 // @desc Delete Goals
 // @route delete /api/goal/:id
 // @access Private
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal... ${req.params.id}` });
-};
-
+});
 
 module.exports = {
   getGoals,
