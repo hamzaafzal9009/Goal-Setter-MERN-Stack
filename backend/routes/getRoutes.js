609074a53c40be1549094a78dd85622f2514ad8b
hamzaafzal <hamza@syncitgroup.com>
2022-08-08 18:01:13 +0500

    routes updated

--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {getGoals, getGoal, setGoal, updateGoal, deleteGoal}  = require("../controllers/goalController")
 
-router.get("/", getGoals);
-router.get("/:id", getGoal);
-router.post("/", setGoal);
-router.put("/:id", updateGoal);
-router.delete("/:id", deleteGoal);
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').get(getGoal).put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
