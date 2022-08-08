46386190ed819b54bf21a0ce9449313b631ef7ee
hamzaafzal <hamza@syncitgroup.com>
2022-08-08 17:57:50 +0500

    Goals controller created

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,54 @@
+// @desc Get Goals
+// @route GET /api/goals/
+// @access Private
+
 const getGoals = (req, res) => {
-    res.status(200).json({ message: "Getting goals..." });
+  res.status(200).json({ message: "Getting goals..." });
 };
+
+
+// @desc Get Goals by ID
+// @route GET /api/goals/:id
+// @access Private
+
+const getGoal = (req, res) => {
+  res.status(200).json({ message: `get goal... ${req.params.id}` });
+};
+
+
+// @desc Set Goals
+// @route POST /api/goals
+// @access Private
+
+const setGoal = (req, res) => {
+  res.status(200).json({ message: `Setting goal...}` });
+};
+
+
+
+// @desc Update Goals
+// @route Put /api/goals/id
+// @access Private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Updating goal... ${req.params.id}}` });
+};
+
+
+
+// @desc Delete Goals
+// @route delete /api/goal/:id
+// @access Private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal... ${req.params.id}` });
+};
+
 
 module.exports = {
   getGoals,
+  getGoal,
+  setGoal,
+  deleteGoal,
+  updateGoal,
 };
--- backend/routes/getRoutes.js
+++ backend/routes/getRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getGoals}   = require("../controllers/goalController")
+const {getGoals, getGoal, setGoal, updateGoal, deleteGoal}  = require("../controllers/goalController")
 
 router.get("/", getGoals);
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "setting goals..." });
-});
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `get goals... ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `deleting goal... ${req.params.id}` });
-});
+router.get("/:id", getGoal);
+router.post("/", setGoal);
+router.put("/:id", updateGoal);
+router.delete("/:id", deleteGoal);
 
 module.exports = router;
